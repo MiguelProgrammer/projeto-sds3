01c503a88fab4c223bcde23c35079fa08c275775
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-06 21:29:04 -0300

    Gráficos com dados estáticos/mock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,35 @@ import './App.css';
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className='container'>
-        <h1 className='text-primary'>Olá mundo!</h1>
+        <h1 className='text-primary py-3'>Dashboard - Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">
+              Taxa de Sucesso
+            </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">
+              Vendas Atuais
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Vendas Atuais</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
