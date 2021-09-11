58908c868224960176d788c4b471d29a7e595564
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2021-09-11 03:13:09 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,8 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+
+/*
+ ?? -> O operador de coalescência nula (??) é um operador 
+    lógico que retorna o seu operando do lado direito quando o seu operador 
+    do lado esquerdo é null ou undefined. Caso contrário, ele retorna o 
+    seu operando do lado esquerdo.
+*/
